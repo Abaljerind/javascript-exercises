6889bfc03ac5056c5933f3080638b6817280b4c4
abaljerind <abaljerinda@gmail.com>
2024-10-17 09:42:25 +0700

    Add 2 parameters, re-declare str with empty string
    Add for loop to repeat the str parameter due to num parameter

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  str = "";
+  for (let i = 0; i < num; i++) {
+    str += "hey";
+  }
+  return str;
 };
 
+console.log(repeatString("hey", 5));
 // Do not edit below this line
 module.exports = repeatString;
