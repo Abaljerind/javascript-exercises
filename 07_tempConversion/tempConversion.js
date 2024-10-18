ea70e9bb3bb13ded78180cfd8485b6cc4dd8acf7
abaljerind <abaljerinda@gmail.com>
2024-10-18 09:31:11 +0700

    Add parameter temperature to each function
    Return the formula for each conversion
    Display the result using console.log

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  return +(((temperature - 32) * 5) / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  return +((temperature * 9) / 5 + 32).toFixed(1);
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToCelsius(100));
+console.log(convertToCelsius(-100));
+console.log(convertToFahrenheit(0));
+console.log(convertToFahrenheit(73.2));
+console.log(convertToFahrenheit(-10));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
