cfd62d798024e1bf95e28619c36f8440042b875b
abaljerind <abaljerinda@gmail.com>
2024-10-18 09:21:51 +0700

    Add parameter year to the function
    Create 3 variable and return a conditional statement

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const yearsDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const yearsDivisibleByFourHundred = year % 400 === 0;
 
+  return yearsDivisibleByFour && (!isCentury || yearsDivisibleByFourHundred);
 };
+
+console.log(leapYears(1996));
+console.log(leapYears(1997));
+console.log(leapYears(34992));
+console.log(leapYears(1900));
+console.log(leapYears(1600));
+console.log(leapYears(700));
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
