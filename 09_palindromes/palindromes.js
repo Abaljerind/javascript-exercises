6674eafe59d3ff18369d8bad13bbeb5665ecd593
Abaljerind <abaljerinda@gmail.com>
2024-11-21 15:14:49 +0700

    Fill up function palindromes with parameter
    Then create variable noPunctuation based on str
    Change all text to lower case
    Last, Check if the reversed variable has the same value of the original
    Check via console for the result

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const noPunctuation = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return noPunctuation.split("").reverse().join("") === noPunctuation;
 };
+
+console.log(palindromes("racecar"));
+console.log(palindromes("ZZZZ car, a man, a maracaz."));
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
