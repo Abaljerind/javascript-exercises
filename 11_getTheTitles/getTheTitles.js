0d7bcf590df6a021a97468e66a96e6a7ac32066e
Abaljerind <abaljerinda@gmail.com>
2024-11-21 16:59:53 +0700

    Add parameter for getTheTitles()
    Create bookTitle with empty array
    Use for of to get the title from the object
    Then push it to bookTitle

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+const getTheTitles = function (obj) {
+  let bookTitle = [];
+  for (const title of obj) {
+    bookTitle.push(title.title);
+  }
 
+  return bookTitle;
 };
+
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
