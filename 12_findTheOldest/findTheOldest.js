32ae2a232e4751889b3dc97d366398fa20a7d02e
Abaljerind <abaljerinda@gmail.com>
2024-11-21 17:26:11 +0700

    Add parameter to findTheOldest() & codes
    Create currentYear variable from Date()
    Using reduce, find the oldest person and each age of the person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,68 @@
-const findTheOldest = function() {
+const findTheOldest = function (obj) {
+  const currentYear = new Date().getFullYear();
 
+  return obj.reduce((oldest, person) => {
+    const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+    return personAge > oldestAge ? person : oldest;
+  });
 };
 
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+const people2 = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+const people3 = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+console.log(findTheOldest(people));
+console.log(findTheOldest(people2));
+console.log(findTheOldest(people3));
 // Do not edit below this line
 module.exports = findTheOldest;
